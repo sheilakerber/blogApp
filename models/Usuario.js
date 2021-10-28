3ef1365d02941da3d533e8e9713b67feed0ffa37
Sheila Kerber <sheila.skerber@gmail.com>
2021-10-28 16:21:14 -0300

    cria campo admin em Usuario.js

--- models/Usuario.js
+++ models/Usuario.js
@@ -10,6 +10,10 @@ const Usuario = new Schema({
         type: String,
         required: true
     }, 
+    eAdmin: {
+        type: Number,
+        default: 0  // 0 = nao Admin; 1 = Admin
+    },
     senha: {
         type: String,
         required: true
